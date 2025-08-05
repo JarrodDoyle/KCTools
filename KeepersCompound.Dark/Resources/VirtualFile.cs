2ef8cfbbea32be4493c92036c90d1582ddcc12b2
Jarrod Doyle <thejarroddoyle@gmail.com>
2025-08-05 18:52:25 +0100

    Split OsVirtualFile out

--- KeepersCompound.Dark/Resources/VirtualFile.cs
+++ KeepersCompound.Dark/Resources/VirtualFile.cs
@@ -2,22 +2,6 @@ using System.IO.Compression;
 
 namespace KeepersCompound.Dark.Resources;
 
-public class OsVirtualFile : BaseVirtualFile
-{
-    public string OsPath { get; init; }
-
-    public OsVirtualFile(string virtualPath, string osPath) : base(virtualPath)
-    {
-        OsPath = osPath;
-    }
-
-    public override MemoryStream GetMemoryStream()
-    {
-        var bytes = File.ReadAllBytes(OsPath);
-        return new MemoryStream(bytes, 0, bytes.Length, true, true);
-    }
-}
-
 public class ZipVirtualFile : BaseVirtualFile
 {
     private readonly ZipArchive _archive;
