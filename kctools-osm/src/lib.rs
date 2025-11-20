d0483319a5a075d227983ae8c38c5871f2ef860a
Jarrod Doyle <thejarroddoyle@gmail.com>
2025-11-20 17:40:33 +0000

    feat: add config validation and avoid naked unwraps

--- kctools-osm/src/lib.rs
+++ kctools-osm/src/lib.rs
@@ -1,23 +1,58 @@
-use std::process::Command;
+use std::{process::Command, result::Result};
 
 use kc_osm::*;
 
 #[unsafe(no_mangle)]
-pub extern "Rust" fn module_init(_: &mut ScriptModule) {
+pub extern "Rust" fn module_init(_: &mut ScriptModule) -> Result<(), &'static str> {
     let services = services();
-    let fm = services.version.get_current_fm().unwrap();
 
-    let output = Command::new("Tools\\KCTools\\KCTools.exe")
+    let mode = match services.engine.config_get_raw("kctools_run") {
+        Some(mode) => mode,
+        None => return Err("'kctools_run' config value not defined."),
+    };
+
+    if mode != "light" {
+        return Err("Unknown 'kctools_run' config value.");
+    }
+
+    let fm = match services.version.get_current_fm() {
+        Some(fm) => fm,
+        None => {
+            return Err(
+                "Failed to determine current FM. Make sure you are running DromEd in FM mode!",
+            );
+        }
+    };
+
+    let path = match services.engine.config_get_raw("kctools_path") {
+        Some(p) => p,
+        None => "Tools\\KCTools\\KCTools.exe".to_owned(),
+    };
+
+    services
+        .debug
+        .print(&format!("Running KCTools with path: {path}"));
+
+    let output = match Command::new(path)
         .arg("light")
         .arg(".")
         .arg("KCLight.cow")
         .arg("-c")
         .arg(fm)
         .output()
-        .unwrap();
+    {
+        Ok(output) => output,
+        Err(_) => return Err("Failed to run KCTools."),
+    };
 
     let output_lines = String::from_utf8_lossy(&output.stdout);
     for line in output_lines.lines() {
         services.debug.print(&line[13..]);
     }
+
+    if !output.status.success() {
+        return Err("Error occured while running KCTools.");
+    }
+
+    Ok(())
 }
